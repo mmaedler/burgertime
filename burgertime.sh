1862834bfc8edc488c6b8056dbb5a22f3f6e7ecf
Moritz Maedler <mmaedler@macbook-df8v.local>
2013-04-04 19:23:47 +0200

    Initial commit

---
+++ burgertime.sh
@@ -0,0 +1,13 @@
+# burger time!
+prompt_bt() {
+	NOON=`date -j -f "%Y-%m-%d %H:%M:%S" "$(date '+%Y-%d-%m 13:00:00')" "+%s"`
+	NOW=`date +%s`
+
+	if [ "$NOON" -lt "$NOW" ]; then
+		NOON=$((NOON+86400))
+	fi
+	DIFF=$((NOON-NOW))
+	printf -v BTIME ""%dh:%dm:%ds"" $(($DIFF/3600)) $(($DIFF%3600/60)) $(($DIFF%60))
+	export PS1="\h:\W (🍔 -time in $BTIME)$ "	
+}
+PROMPT_COMMAND=prompt_bt
